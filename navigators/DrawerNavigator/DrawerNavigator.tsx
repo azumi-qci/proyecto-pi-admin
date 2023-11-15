060e5ec59f3e990e373dcd42afef45b245b1967f
Alejandro Suárez <alejandro-hdez115@outlook.com>
2023-11-14 20:25:21 -0600

    Update drawer design

--- navigators/DrawerNavigator/DrawerNavigator.tsx
+++ navigators/DrawerNavigator/DrawerNavigator.tsx
@@ -1,21 +1,66 @@
 import { FC } from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import {
+  createDrawerNavigator,
+  DrawerContentComponentProps,
+  DrawerContentScrollView,
+  DrawerItemList,
+} from '@react-navigation/drawer';
+import { Text, useTheme } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native';
 
 import { HomeScreen } from '../../screens/HomeScreen';
-import { View } from 'react-native';
 
 const Drawer = createDrawerNavigator();
 
+interface CustomDrawerProps extends DrawerContentComponentProps {
+  email: string;
+}
+
 const DrawerNavigator: FC = () => {
   return (
-    <Drawer.Navigator initialRouteName="Home">
+    <Drawer.Navigator
+      initialRouteName="Home"
+      drawerContent={props => (
+        <CustomDrawer email="alejandro-hdez115@outlook.com" {...props} />
+      )}>
       <Drawer.Screen
         name="Home"
         component={HomeScreen}
-        options={{ headerTitle: 'Monitoreo de entrada', title: 'Inicio' }}
+        options={{ headerTitle: 'Monitoreo de entradas', title: 'Inicio' }}
       />
     </Drawer.Navigator>
   );
 };
+
+const CustomDrawer: FC<CustomDrawerProps> = ({ email, ...props }) => {
+  const theme = useTheme();
+
+  return (
+    <DrawerContentScrollView {...props}>
+      <View
+        style={[
+          styles.drawerHeaderContainer,
+          { borderBottomColor: theme.colors.shadow },
+        ]}>
+        <Text style={styles.headerTextBold}>Correo electrónico</Text>
+        <Text>{email}</Text>
+      </View>
+      <DrawerItemList {...props} />
+    </DrawerContentScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  drawerHeaderContainer: {
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    paddingTop: 15,
+    paddingBottom: 20,
+  },
+  headerTextBold: {
+    fontWeight: 'bold',
+  },
+});
 
 export default DrawerNavigator;
