eedaa431f3f395f60dec63dd2fa97be300e9f582
Alejandro Suárez <alejandro-hdez115@outlook.com>
2023-11-19 19:03:29 -0600

    Add access log delete action

--- screens/DoorDataScreen/DoorDataScreen.tsx
+++ screens/DoorDataScreen/DoorDataScreen.tsx
@@ -1,8 +1,14 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { useRoute } from '@react-navigation/native';
 import { FC, useEffect, useState } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
-import { List, Text, TouchableRipple } from 'react-native-paper';
+import { FlatList, StyleSheet, ToastAndroid, View } from 'react-native';
+import {
+  Dialog,
+  Portal,
+  Text,
+  TouchableRipple,
+  Button,
+} from 'react-native-paper';
 
 import { DoorDataScreenRouteProp } from '../../types/navigation.types';
 import { Log } from '../../types/data.types';
@@ -15,6 +21,8 @@ const DoorDataScreen: FC = () => {
   const [loading, setLoading] = useState(true);
   const [token, setToken] = useState<string | null>(null);
   const [logs, setLogs] = useState<Log[] | null>(null);
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  const [logToDelete, setLogToDelete] = useState<number | null>(null);
 
   const getLogs = async () => {
     api
@@ -46,7 +54,47 @@ const DoorDataScreen: FC = () => {
   };
 
   const onLongPressLog = (id: number) => {
-    console.log(id);
+    setLogToDelete(id);
+    setShowDeleteDialog(true);
+  };
+
+  const onCancelDeleteLog = () => {
+    setLogToDelete(null);
+    setShowDeleteDialog(false);
+  };
+
+  const onConfirmDeleteLog = () => {
+    api
+      .delete(`/access/${logToDelete}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then(() => {
+        const itemIndex = logs?.findIndex(item => item.id === logToDelete);
+
+        if (logs && itemIndex && itemIndex > -1) {
+          const tempList = [...logs];
+
+          tempList.splice(itemIndex, 1);
+
+          setLogs(tempList);
+        }
+        // Show message
+        ToastAndroid.show(
+          'Se eliminó el registro correctamente',
+          ToastAndroid.SHORT,
+        );
+      })
+      .catch(error => {
+        console.warn(error);
+        // Alert error
+        ToastAndroid.show(
+          'Hubo un error al eliminar el registro',
+          ToastAndroid.SHORT,
+        );
+      })
+      .then(() => onCancelDeleteLog());
   };
 
   useEffect(() => {
@@ -72,31 +120,48 @@ const DoorDataScreen: FC = () => {
   }
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title} variant="titleLarge">
-        {name}
-      </Text>
-      <FlatList
-        data={logs}
-        renderItem={({ item }) => (
-          <TouchableRipple
-            onPress={() => onPressLog(item.id)}
-            onLongPress={() => onLongPressLog(item.id)}
-            rippleColor="rgba(0, 0, 0, .32)">
-            <View style={styles.itemContainer}>
-              <Text style={styles.itemTitle}>{item.name}</Text>
-              <Text>
-                {getTime(item.access_daytime)} | {getDate(item.access_daytime)}
-              </Text>
-              <Text>
-                {item.car_brand} {item.car_color} {item.car_plate}
-              </Text>
-              <Text>{item.visit_location}</Text>
-            </View>
-          </TouchableRipple>
-        )}
-      />
-    </View>
+    <>
+      {/* Dialog */}
+      <Portal>
+        <Dialog visible={showDeleteDialog} onDismiss={onCancelDeleteLog}>
+          <Dialog.Title>Eliminar registro</Dialog.Title>
+          <Dialog.Content>
+            <Text>Está por eliminar el registro, ¿desea eliminar?</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={onCancelDeleteLog}>Cancelar</Button>
+            <Button onPress={onConfirmDeleteLog}>Eliminar</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+      {/* Content */}
+      <View style={styles.container}>
+        <Text style={styles.title} variant="titleLarge">
+          {name}
+        </Text>
+        <FlatList
+          data={logs}
+          renderItem={({ item }) => (
+            <TouchableRipple
+              onPress={() => onPressLog(item.id)}
+              onLongPress={() => onLongPressLog(item.id)}
+              rippleColor="rgba(0, 0, 0, .32)">
+              <View style={styles.itemContainer}>
+                <Text style={styles.itemTitle}>{item.name}</Text>
+                <Text>
+                  {getTime(item.access_daytime)} |{' '}
+                  {getDate(item.access_daytime)}
+                </Text>
+                <Text>
+                  {item.car_brand} {item.car_color} {item.car_plate}
+                </Text>
+                <Text>{item.visit_location}</Text>
+              </View>
+            </TouchableRipple>
+          )}
+        />
+      </View>
+    </>
   );
 };
 
